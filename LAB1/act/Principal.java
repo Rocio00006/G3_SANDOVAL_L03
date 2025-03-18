8d45601e09841f8bec25ae8df44070ddc3890ec6
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-18 16:51:12 -0500

    clase principal y verificador

---
+++ LAB1/act/Principal.java
@@ -0,0 +1,15 @@
+package LAB1.act;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        double x1r1, x2r1, y1r1, y2r1;
+        double x1r2, x2r2, y1r2, y2r2;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una esquina del 1° rectangulo: ");
+        x1r1 = sc.nextDouble();
+        y1r1 = sc.nextDouble();
+        System.out.println("Ingrese la esquina opuesta del 1er rectangulo: ");
+    }
+}
--- LAB1/act/Verificador.java
+++ LAB1/act/Verificador.java
@@ -1,4 +1,4 @@
-import lab01.act.Rectangulo;
+package LAB1.act;
 
 public class Verificador {
     public static boolean esSobrePos(LAB1.act.Rectangulo r1, Rectangulo r2){
@@ -17,6 +17,12 @@ public class Verificador {
         r2y1 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
         r2y2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
 
-        return true;    
+        //condiciones para que se sobrepongan
+        if(r1x1<r2x1 || r1x2<r2x2 || r1y1<r2y2 || r1y2<r2y2 ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
