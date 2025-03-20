f26f605f1881be848e35b427c63948d034d3b1f4
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-20 07:16:52 -0500

    Verificador

--- LAB1/act/Verificador.java
+++ LAB1/act/Verificador.java
@@ -1,49 +1,99 @@
 package LAB1.act;
 
 public class Verificador {
-    public static boolean esSobrePos(LAB1.act.Rectangulo r1, Rectangulo r2){
-        double r1x1, r1y1, r1x2, r1y2;  //esquinas de r1
-        double r2x1, r2y1, r2x2, r2y2;  //esquinas de r2
 
-        //definir x1, x2, y1, y2 de r1
-        r1x1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
-        r1x2 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
-        r1y1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
-        r1y2 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+
+        double x1r1, y1r1, x2r1, y2r1;
+        double x1r2, y1r2, x2r2, y2r2;
+
+        //asignar valores rectangulo 1
+        x1r1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        x2r1 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        y1r1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+        y2r1 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
         
-        //definir x1, x2, y1, y2 de r2
-        r2x1 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
-        r2x2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
-        r2y1 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
-        r2y2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        //asignar valores rectangulo 2
+        x1r2 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        x2r2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        y1r2 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        y2r2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
 
         //condiciones para que se sobrepongan
-        //no hay intersección si r1 está arriba, debajo, a la izquierda o derecha de r2
-        if(r1y2 < r2y1 || r1y1>r2y2 || r1x2<r2x1 || r1x1>r2x2){
+        //no hay intersección si r1 está a ña derecho, izquierdo, arriba o debajo de r2
+        if(x1r1 >=x2r2 || x2r1 <= x1r2 || y1r1 >= y2r2 || y2r1 <= y1r2){
             return false;
         }
         else
             return true;
-
     }
-    public static boolean esJunto(Rectangulo r1, Rectangulo r2){
 
-        return true;
+    public static boolean esJunto(Rectangulo r1, Rectangulo r2){
+        if (esSobrePos(r1, r2)) {
+            return false;
+        }
+
+        double x1r1, y1r1, x2r1, y2r1;
+        double x1r2, y1r2, x2r2, y2r2;
+        
+        //establecer esquinas de r1
+        x1r1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        x2r1 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        y1r1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+        y2r1 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+
+        //establecer esquinas del rectángulo r2
+        x1r2 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        x2r2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        y1r2 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        y2r2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+
+        //para que se "junten" deben compartir un lado vertical u horizontal o esquinas
+        boolean ladoVertical =false;
+        boolean ladoHorizontal =false;
+
+        //comparten lado vertical o un punto en el eje vertical
+        if ((x1r1 == x2r2 || x2r1 == x1r2) && !(y1r1 > y2r2 || y2r1 < y1r2)){
+            ladoVertical = true;
+        }
+        //comparten el lado horizontal o un punto en el eje horizontal
+        if((y2r1 == y1r2 || y1r1 == y2r2) && !(x1r1 > x2r2 || x2r1 < x1r2)){
+            ladoHorizontal = true;
+        }
+        
+        if(ladoVertical==true || ladoHorizontal==true){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     public static boolean esDisjunto(Rectangulo r1, Rectangulo r2){
-        double r1x1, r1y1, r1x2, r1y2;  //esquinas de r1
-        double r2x1, r2y1, r2x2, r2y2;  //esquinas de r2
-        //definir x1, x2, y1, y2 de r1
-        r1x1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
-        r1x2 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
-        r1y1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
-        r1y2 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+
+        if(esJunto(r1, r2)==true){
+            return false;
+        }
+        double x1r1, y1r1, x2r1, y2r1;
+        double x1r2, y1r2, x2r2, y2r2;
         
-        //definir x1, x2, y1, y2 de r2
-        r2x1 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
-        r2x2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
-        r2y1 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
-        r2y2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
-        return true;
+        // Establecer esquinas de r1
+        x1r1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        x2r1 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        y1r1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+        y2r1 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+
+        // Establecer esquinas del rectángulo r2
+        x1r2 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        x2r2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        y1r2 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        y2r2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+
+        //condiciones para que se sobrepongan
+        //no hay intersección si r1 está a ña derecho, izquierdo, arriba o debajo de r2
+        if(x1r1 > x2r2 || x2r1 < x1r2 || y2r1 < y1r2 || y1r1 > y2r2){
+            return true;
+        }
+        else
+            return false;
     }
 }
