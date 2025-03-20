eb1623b539e9ef987559a9da76129deffb3869dd
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-20 04:20:17 -0500

    sobrePos

--- LAB1/act/Principal.java
+++ LAB1/act/Principal.java
@@ -10,6 +10,32 @@ public class Principal {
         System.out.println("Ingrese una esquina del 1° rectangulo: ");
         x1r1 = sc.nextDouble();
         y1r1 = sc.nextDouble();
-        System.out.println("Ingrese la esquina opuesta del 1er rectangulo: ");
+        System.out.println("Ingrese la esquina opuesta del 1° rectangulo: ");
+        x2r1 = sc.nextDouble();
+        y2r1 = sc.nextDouble();
+
+        System.out.println("Ingrese una esquina del 2° rectangulo: ");
+        x1r2 = sc.nextDouble();
+        y1r2 = sc.nextDouble();
+        System.out.println("Ingrese la esquina opuesta del 2° rectangulo: ");
+        x2r2 = sc.nextDouble();
+        y2r2 = sc.nextDouble();
+
+        Coordenada e1a = new Coordenada(x1r1, y1r1);
+        Coordenada e2a = new Coordenada(x2r1, y2r1);
+
+        Coordenada e1b = new Coordenada(x1r2, y1r2);
+        Coordenada e2b = new Coordenada(x2r2, y2r2);
+
+        Rectangulo rA = new Rectangulo(e1a, e2a);
+        Rectangulo rB = new Rectangulo(e1b, e2b);
+
+        if(Verificador.esSobrePos(rA, rB)== true){
+            System.out.println("se sobreponene");
+        }
+        else
+            System.out.println("no se sobreponen");
+
+        sc.close();
     }
 }
--- LAB1/act/Verificador.java
+++ LAB1/act/Verificador.java
@@ -18,11 +18,32 @@ public class Verificador {
         r2y2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
 
         //condiciones para que se sobrepongan
-        if(r1x1<r2x1 || r1x2<r2x2 || r1y1<r2y2 || r1y2<r2y2 ){
-            return true;
-        }
-        else{
+        //no hay intersección si r1 está arriba, debajo, a la izquierda o derecha de r2
+        if(r1y2 < r2y1 || r1y1>r2y2 || r1x2<r2x1 || r1x1>r2x2){
             return false;
         }
+        else
+            return true;
+
+    }
+    public static boolean esJunto(Rectangulo r1, Rectangulo r2){
+
+        return true;
+    }
+    public static boolean esDisjunto(Rectangulo r1, Rectangulo r2){
+        double r1x1, r1y1, r1x2, r1y2;  //esquinas de r1
+        double r2x1, r2y1, r2x2, r2y2;  //esquinas de r2
+        //definir x1, x2, y1, y2 de r1
+        r1x1 = Math.min(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        r1x2 = Math.max(r1.getEsquina1().getX(), r1.getEsquina2().getX());
+        r1y1 = Math.min(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+        r1y2 = Math.max(r1.getEsquina1().getY(), r1.getEsquina2().getY());
+        
+        //definir x1, x2, y1, y2 de r2
+        r2x1 = Math.min(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        r2x2 = Math.max(r2.getEsquina1().getX(), r2.getEsquina2().getX());
+        r2y1 = Math.min(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        r2y2 = Math.max(r2.getEsquina1().getY(), r2.getEsquina2().getY());
+        return true;
     }
 }
