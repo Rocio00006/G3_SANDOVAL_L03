b9e5602f1ff7b2e7a682e0c80c17bc977980ce8f
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-25 16:20:54 -0500

    clase chocolatina

---
+++ LAB2/act/Chocolatina.java
@@ -0,0 +1,17 @@
+package LAB2.act;
+
+public class Chocolatina {
+   private String marca;
+   
+   //constructor
+   public Chocolatina(String marca){
+    this.marca = marca;
+   }
+   public String getMarca(){
+    return marca;
+   }
+   public void setMarca(String marca){
+    this.marca = marca;
+   }
+}
+
