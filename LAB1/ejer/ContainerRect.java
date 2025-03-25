205a955d2c9412fcbb1277a8b2fc8547974c14a6
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-25 09:06:49 -0500

    clase container

--- LAB1/ejer/ContainerRect.java
+++ LAB1/ejer/ContainerRect.java
@@ -5,20 +5,20 @@ public class ContainerRect {
     private double [] arregloDistancias;
     private double[] arregloAreas;
     private int n; //número máximo de rectángulos
-    private int numRec; //contador 
+    public static int numRec; //contador variable de clase 
 
     //constructor
-    public ContainerRect(int cant){
-        this.arregloRec = new Rectangulo[cant]; //inicializar arreglo
-        this.arregloDistancias = new double[cant];
-        this.arregloAreas = new double[cant];
-        this.numRec = 0;
-        this.n = cant;
+    public ContainerRect(int n){
+        this.n = n;
+        this.arregloRec = new Rectangulo[n]; //inicializar arreglo
+        this.arregloDistancias = new double[n];
+        this.arregloAreas = new double[n];
+        //this.numRec = 0;
     }
 
     //metodos setters y getters
     public Rectangulo[] getArregloRec() {
-        for(int i=0; i<n;i++){
+        for(int i=0; i<arregloRec.length;i++){
             System.out.println(arregloRec[i].toString());
         }
         return arregloRec;
@@ -44,22 +44,31 @@ public class ContainerRect {
     public void setN(int n) {
         this.n = n;
     }
-    public int getNumRec() {
-        return numRec;
-    }
-    public void setNumRec(int numRec) {
-        this.numRec = numRec;
-    }
     public void addRectangulo(Rectangulo r1){
         //verificar que haya espacio en el arreglo
         if(numRec<n){
-            for(int i=0; i<n;i++){
-                
+            double areaRec = r1.calculoArea();
+            double distanciaRec = Coordenada.distancia(r1.getEsquina1(), r1.getEsquina2());
+
+            for(int i=0; i<numRec;i++){
+                arregloRec[i] = r1;
+                arregloAreas[i] = areaRec;
+                arregloDistancias[i] = distanciaRec;
+                numRec++;
             }
+            System.out.println("Rectangulo agregado");
         }
         else{
-            System.out.println("el arreglo está full");
+            System.out.println("El arreglo está lleno");
         }
+    }
+    @Override
+    public String toString(){
+        System.out.println("Rectangulo   Coordenadas     Distancia   Area");
+        for(int i = 0; i<numRec; i++){
+            System.out.println((n+1)+"     "+arregloRec[i].toString()+"    "+arregloDistancias[i]+"   "+arregloAreas[i]);
+        }
+        return "";
     }
 
 }
