715c3c7ef634125bffbbb3dc11dfe5279f74201e
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-27 17:41:26 -0500

    método genérico

---
+++ LAB2/act/DemoMetodoGenerico.java
@@ -0,0 +1,35 @@
+package LAB2.act;
+
+public class DemoMetodoGenerico {
+    //definición de método genérico
+    public static <T extends Comparable<T>> boolean igualArrays(T[] x, T[] y){
+        //primero verificar longitud
+        if(x.length!=y.length)
+            return false;
+        //si hay elementos en el arreglo
+        //verificar elementos con la posición i de arreglo x y y
+        for(int i=0; i<x.length; i++)
+            if(!x[i].equals(y[i]))
+                return false;   //arreglos diferentes
+        return true;
+    }
+    public static void main(String[] args) { 
+        Integer nums[]={1,2,3,4,5}; 
+        Integer nums2[]={1,2,3,4,5}; 
+        Integer nums3[]={1,2,7,4,5}; 
+        Integer nums4[]={1,2,7,4,5,6}; 
+        if (igualArrays(nums,nums)) 
+            System.out.println("nums es igual a nums"); 
+        if (igualArrays(nums,nums2)) 
+            System.out.println("nums es igual a nums2"); 
+        if (igualArrays(nums,nums3)) 
+            System.out.println("nums es igual a num3"); 
+        if (igualArrays(nums,nums4)) 
+            System.out.println("nums es igual a nums4"); 
+ 
+        // Crea un array de double    //A 
+        //Double dvals[]={1.1,2.2,3.3,4.4,5.5};  //B 
+        //if(igualArrays(nums,dvals))   //C no se puede hacer la comparación de diferte tipo
+            //System.out.println("nums es igual a dvals"); //D 
+    } 
+}
