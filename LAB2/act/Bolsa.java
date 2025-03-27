85bc80df1d63c436473d80edf29d079cfd2b7ab5
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-27 17:26:22 -0500

    CLASE BOLSA

--- LAB2/act/Bolsa.java
+++ LAB2/act/Bolsa.java
@@ -3,15 +3,17 @@ package LAB2.act;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Bolsa <T> implements Iterator <T>{
+public class Bolsa <T> implements Iterable <T>{
+    //crear objeto de la clase arrayList con tipos de datos genéricos
     private ArrayList<T> lista = new ArrayList<T>();
-    private int tope;
+    private int tope; //variables que define el tamaño del array
 
+    //constructor
     public Bolsa(int tope){
         super();
         this.tope = tope;
     }
-    //método genérico
+    //método genérico que añade objetos de tipo T
     public void add(T objeto){
         if (lista.size()<=tope){
             lista.add(objeto);
@@ -19,8 +21,10 @@ public class Bolsa <T> implements Iterator <T>{
             throw new RuntimeException("no caben más");
         }
     }
-    //método de la interfaz 
+    //método de la interfaz Iterable
     public Iterator <T> iterator(){
         return lista.iterator();
     }
+
+
 }
