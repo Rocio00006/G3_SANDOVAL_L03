5f268e938fbad63d8230c9c84cc212c4139dde38
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-03-28 09:50:22 -0500

    igualArrays y exist

--- LAB2/act/DemoMetodoGenerico.java
+++ LAB2/act/DemoMetodoGenerico.java
@@ -1,20 +1,40 @@
 package LAB2.act;
 
+import java.util.ArrayList;
+
 public class DemoMetodoGenerico {
     //definición de método genérico
     public static <T extends Comparable<T>> boolean igualArrays(T[] x, T[] y){
         //primero verificar longitud
         if(x.length!=y.length)
             return false;
-        //si hay elementos en el arreglo
         //verificar elementos con la posición i de arreglo x y y
         for(int i=0; i<x.length; i++)
             if(!x[i].equals(y[i]))
                 return false;   //arreglos diferentes
         return true;
     }
+
+    //método para ver si existe un elemento en un arreglo de tipo T
+    public static <T> boolean exist(T[] arreglo, T elemento){
+        for(T e: arreglo){
+            if(e.equals(elemento))
+                return true;
+        }
+        return false;
+    }
+
+    //metodo para ver si existe un elemento en un arrayList "generico"
+    public static <T> boolean exist(ArrayList<T> arrayL, T element){
+        for(T e : arrayL){
+            if(e.equals(element))
+                return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) { 
-        Integer nums[]={1,2,3,4,5}; 
+        Integer nums[]={1,2,3,4,5}; //arreglos de tipo Integer
         Integer nums2[]={1,2,3,4,5}; 
         Integer nums3[]={1,2,7,4,5}; 
         Integer nums4[]={1,2,7,4,5,6}; 
@@ -30,6 +50,7 @@ public class DemoMetodoGenerico {
         // Crea un array de double    //A 
         //Double dvals[]={1.1,2.2,3.3,4.4,5.5};  //B 
         //if(igualArrays(nums,dvals))   //C no se puede hacer la comparación de diferte tipo
-            //System.out.println("nums es igual a dvals"); //D 
+        //    System.out.println("nums es igual a dvals"); //D 
     } 
 }
+
