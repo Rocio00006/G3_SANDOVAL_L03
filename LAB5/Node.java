df7edc999a07112015731a3cc6da72bc31f79053
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-04-29 16:25:25 -0500

    clase nodo

---
+++ LAB5/Node.java
@@ -0,0 +1,24 @@
+package LAB5;
+
+public class Node <E> {
+    public E valor;
+    public Node<E> next;
+
+    //constructores
+    public Node(){
+        this.valor= null;
+        this.next = null;
+    }
+    public Node(E valor){
+        this.valor= valor;
+        this.next=null;
+    }
+    public Node(E valor, Node<E> next) {
+        this.valor = valor;
+        this.next = next;
+    }
+    public Node(Node<E> nodo){
+        this.valor = nodo.valor;
+        this.next = nodo.next;
+    }
+}
