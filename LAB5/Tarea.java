0b821991efdb369b7f5f7df2ec8d2f877fdb3bc9
ROCIO <rocio.sandoval@ucsm.edu.pe>
2025-05-05 01:58:13 -0500

    tarea con met

--- LAB5/Tarea.java
+++ LAB5/Tarea.java
@@ -4,4 +4,31 @@ public class Tarea {
     private String titulo;
     private int prioridad;
     
+    //constructor
+    public Tarea(){
+        this.titulo = "";
+        this.prioridad = 0;
+    }
+    public Tarea(String titulo, int prioridad){
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+    }
+    //getters y setters
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getPrioridad() {
+        return prioridad;
+    }
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+    @Override
+    public String toString() {
+        return "Tarea [titulo=" + titulo + ", prioridad=" + prioridad + "]";
+    }
+    
 }
